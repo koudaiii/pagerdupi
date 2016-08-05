4f68ca5c3bb1909e1ccb51bc3579214e31c2c521
koudaiii <cs006061@gmail.com>
2016-08-05 20:41:24 +0900

    Add option sample

--- examples/incidents.rb
+++ examples/incidents.rb
@@ -6,3 +6,10 @@ Dotenv.load
 
 incidents = Pagerdupi::Incident.new(ENV['PAGER_DUTY_API_KEY'])
 puts incidents.list
+
+puts incidents.list(
+  limit: 100,
+  since: (Time.now - 7 * 24 * 60 * 60).strftime('%Y-%m-%d'),
+  until: Time.now.strftime('%Y-%m-%d'),
+  time_zone: 'Asia/Tokyo',
+)
