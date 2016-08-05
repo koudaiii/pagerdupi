4f77dbbbbbf9160252cbe7024c91abe732a6fd88
koudaiii <cs006061@gmail.com>
2016-08-05 19:57:56 +0900

    Add incidents list

--- lib/pagerdupi/incidents.rb
+++ lib/pagerdupi/incidents.rb
@@ -1,14 +1,32 @@
 module Pagerdupi
   class Incident
-    def list
+
+    def initialize(api_key)
+      @pagerdupi = Pagerdupi::Client.new(api_key)
     end
-    def show
+
+    def list(params = {})
+      default_params = {
+        limit: 25,
+        since: (Time.now - 7 * 24 * 60 * 60).strftime('%Y-%m-%d'),
+        until: Time.now.strftime('%Y-%m-%d'),
+        time_zone: 'Asia/Tokyo',
+      }
+      query_params = default_params.merge(params)
+
+      Hashie::Mash.new(@pagerdupi.get('/incidents', query: query_params))
     end
-    def create
+
+    def show(params = {})
     end
-    def update
+
+    def create(params = {})
     end
-    def delete
+
+    def update(params = {})
+    end
+
+    def delete(params = {})
     end
   end
 end
