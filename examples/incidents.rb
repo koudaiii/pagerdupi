877f9ae11590293145308b6ac70d35f40cf7b61e
koudaiii <cs006061@gmail.com>
2016-08-05 20:58:58 +0900

    use hashie for examples

--- examples/incidents.rb
+++ examples/incidents.rb
@@ -1,15 +1,17 @@
 require "rubygems"
 require "pagerdupi"
 require "dotenv"
+require "hashie"
 
 Dotenv.load
 
 incidents = Pagerdupi::Incident.new(ENV['PAGER_DUTY_API_KEY'])
-puts incidents.get_incidents
 
-puts incidents.get_incidents(
+puts Hashie::Mash.new(incidents.get_incidents)
+
+puts Hashie::Mash.new(incidents.get_incidents(
   limit: 100,
   since: (Time.now - 7 * 24 * 60 * 60).strftime('%Y-%m-%d'),
   until: Time.now.strftime('%Y-%m-%d'),
   time_zone: 'Asia/Tokyo',
-)
+))
