98f05cb1842c83009b01aa192e1c1cd4e9dbc476
koudaiii <cs006061@gmail.com>
2016-08-11 14:28:24 +0900

    Add get_maintenance_windows method

--- lib/pagerdupi/maintenance_windows.rb
+++ lib/pagerdupi/maintenance_windows.rb
@@ -1,14 +1,20 @@
 module Pagerdupi
   class MaintenanceWindow
-    def list
+    def initialize(api_key)
+      @pagerdupi = Pagerdupi::Client.new(api_key)
     end
-    def show
-    end
-    def create
-    end
-    def update
-    end
-    def delete
+
+    def get_maintenance_windows(params = {})
+      default_params = {
+       team_ids: [],
+       service_ids: [],
+       include: [],
+       filter: nil,
+       query: nil,
+      }
+      query_params = default_params.merge(params)
+
+      @pagerdupi.get('/maintenance_windows', quary: query_params)
     end
   end
 end
