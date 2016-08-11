cfd8903149f01699c3e4f37e0a1de2dcdbe82138
koudaiii <cs006061@gmail.com>
2016-08-11 14:42:11 +0900

    Add get_notifications method

--- lib/pagerdupi/notifications.rb
+++ lib/pagerdupi/notifications.rb
@@ -1,6 +1,19 @@
 module Pagerdupi
   class Notification
-    def list
+    def initialize(api_key)
+      @pagerdupi = Pagerdupi::Client.new(api_key)
+    end
+
+    def get_notifications(params = {})
+      default_params = {
+        limit: 25,
+        time_zone: 'UTC',
+        since: (Time.now - 7 * 24 * 60 * 60).strftime('%Y-%m-%d'),
+        until: Time.now.strftime('%Y-%m-%d'),
+        filter: nil,
+      }
+      query_params = default_params.merge(params)
+      @pagerdupi.get('/notifications', quary: query_params)
     end
   end
 end
